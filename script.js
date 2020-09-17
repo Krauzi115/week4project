d774df17fff6ca393246a3877294a0b139235a43
Kyle Rauzi <krauzi@lclark.edu>
2020-09-17 10:05:46 -0700

    malena quiz egg

--- script.js
+++ script.js
@@ -1,27 +1,150 @@
-var questions = [
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreDiv = document.getElementById("scoreContainer");
+
+let questions = [
     {
-          prompt: "Space Jam by Quad City DJs the greatest sports anthem of all time.\n(a) True\n(b) False\n",
-          answer: "a"
-    },
-    {
-         prompt: "Fkj & Masego's hit song Tadow drew inspiration from YungFlexMaster Rauzi \n(a) True\n(b) False\n",
-         answer: "b"
-    },
-    {
-         prompt: "Terra likes EDM and Lofi?\n(a) True\n(b) False\n",
-         answer: "a"
+        question : "The fatser and more uplifitng the song, the better?",
+        imgSrc : "img/happyface.jpg",
+        choiceA : "True",
+        choiceB : "False",
+        choiceC : "I'll have fries with that",
+        correct : "B"
+    },{
+        question : "Malena flexes on most Lizzo fans by being a top 5 percent spotify listener?",
+        imgSrc : "img/lizzo.jpg",
+        choiceA : "True",
+        choiceB : "False",
+        choiceC : "Just took a DNA test turns out i'm 100% a Cardi B listener",
+        correct : "A"
+    },{
+        question : "Malena enjoys bluegrass and women indie singers?",
+        imgSrc : "img/bgrass.jpg",
+        choiceA : "False",
+        choiceB : "Wait grass is blue?",
+        choiceC : "True",
+        correct : "C"
     }
 ];
-var score = 0;
 
 
-for(var i = 0; i < questions.length; i++){
-    var response = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-         score++;
-         alert("Correct!");
-    } else {
-         alert("WRONG!");
+
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; 
+const gaugeWidth = 150; 
+const gaugeUnit = gaugeWidth / questionTime;
+let TIMER;
+let score = 0;
+
+
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click",startQuiz);
+
+
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000); 
+}
+
+
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
     }
 }
-alert("you got " + score + "/" + questions.length);
+
+
+
+function renderCounter(){
+    if(count <= questionTime){
+        counter.innerHTML = count;
+        timeGauge.style.width = count * gaugeUnit + "px";
+        count++
+    }else{
+        count = 0;
+        
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        
+        score++;
+        
+        answerIsCorrect();
+    }else{
+        
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+
+function answerIsCorrect(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+
+function answerIsWrong(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+    
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    
+    let img = (scorePerCent >= 80) ? "img/5.png" :
+              (scorePerCent >= 60) ? "img/4.png" :
+              (scorePerCent >= 40) ? "img/3.png" :
+              (scorePerCent >= 20) ? "img/2.png" :
+              "img/1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+}
